bf3213967503e94cdc2261bb1dd7b9881099bc43
lonmstalker <nkochnev@zuzex.com>
2021-12-13 16:52:21 +0300

    added two multithreading tests

--- dsl-abac-service/src/test/kotlin/io/github/sevenparadigms/dslabac/api/context/ExchangeHolderIntegrationTest.kt
+++ dsl-abac-service/src/test/kotlin/io/github/sevenparadigms/dslabac/api/context/ExchangeHolderIntegrationTest.kt
@@ -4,6 +4,8 @@ import io.github.sevenparadigms.abac.Constants
 import io.github.sevenparadigms.dslabac.AbstractIntegrationTest
 import org.junit.jupiter.api.Test
 import org.springframework.http.HttpHeaders
+import reactor.core.publisher.Flux
+import reactor.core.scheduler.Schedulers
 import reactor.test.StepVerifier
 
 class ExchangeHolderIntegrationTest : AbstractIntegrationTest() {
@@ -19,8 +21,72 @@ class ExchangeHolderIntegrationTest : AbstractIntegrationTest() {
 
         StepVerifier.create(mono)
             .expectNextMatches {
-                it.stream().allMatch{ value -> value != null } &&
-                it[1] == correctIp && it[3] == adminToken
+                it.stream().allMatch { value -> value != null } &&
+                        it[1] == correctIp && it[3] == adminToken
+            }
+            .thenCancel()
+            .verify()
+    }
+
+    @Test
+    fun `test Holder race condition by one user`() {
+        var lastList: List<*>? = null
+
+        val flux = Flux.range(1, 50)
+            .parallel(10)
+            .runOn(Schedulers.boundedElastic())
+            .flatMap { rangeCount ->
+                webClient.get()
+                    .uri("dsl-abac/context")
+                    .header(HttpHeaders.AUTHORIZATION, Constants.BEARER + adminToken)
+                    .header(Constants.AUTHORIZE_IP, correctIp + rangeCount)
+                    .exchangeToMono { it.bodyToMono(List::class.java) }
+                    .map { it as List<*> }
+                    .doOnNext { lastList = it }
+            }
+
+        StepVerifier.create(flux)
+            .expectNextCount(49)
+            .expectNextMatches { lastList?.get(1) == it[1] }
+            .thenCancel()
+            .verify()
+    }
+
+    @Test
+    fun `test Holder race condition by two users`() {
+        var firstUserList: List<*>? = null
+        var secondUserList: List<*>? = null
+
+        val flux = Flux.range(1, 100)
+            .parallel(10)
+            .runOn(Schedulers.boundedElastic())
+            .flatMap { rangeCount ->
+                if (rangeCount % 2 == 0) {
+                    webClient.get()
+                        .uri("dsl-abac/context")
+                        .header(HttpHeaders.AUTHORIZATION, Constants.BEARER + adminToken)
+                        .header(Constants.AUTHORIZE_IP, nonCorrectIp + rangeCount)
+                        .exchangeToMono { it.bodyToMono(List::class.java) }
+                        .map { it as List<*> }
+                        .doOnNext{ secondUserList = it }
+                } else {
+                    webClient.get()
+                        .uri("dsl-abac/context")
+                        .header(HttpHeaders.AUTHORIZATION, Constants.BEARER + userToken)
+                        .header(Constants.AUTHORIZE_IP, correctIp + rangeCount)
+                        .exchangeToMono { it.bodyToMono(List::class.java) }
+                        .doOnNext { firstUserList = it }
+                }
+            }
+            .sequential()
+            .collectList()
+
+        StepVerifier.create(flux)
+            .expectNextMatches {
+                firstUserList?.get(1) == it.reversed().stream().filter { list -> list[3] == userToken }.findFirst()
+                    .get()[1] &&
+                        secondUserList?.get(1) == it.reversed().stream().filter { list -> list[3] == adminToken }
+                    .findFirst().get()[1]
             }
             .thenCancel()
             .verify()
