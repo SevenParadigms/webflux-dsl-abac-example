a8966a772b11f752272922779000e9e7cb80654a
Lao Tsing <lao.tsing@outlook.com>
2022-04-21 18:44:18 +0300

    up depends

--- dsl-abac-service/build.gradle.kts
+++ dsl-abac-service/build.gradle.kts
@@ -1,11 +1,11 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "2.6.5"
+	id("org.springframework.boot") version "2.6.6"
 	id("io.spring.dependency-management") version "1.0.11.RELEASE"
 	id("org.liquibase.gradle") version "2.1.1"
-	kotlin("jvm") version "1.6.10"
-	kotlin("plugin.spring") version "1.6.10"
+	kotlin("jvm") version "1.6.21"
+	kotlin("plugin.spring") version "1.6.21"
 }
 
 tasks.withType<Test> {
